7dcd5482e22904856f062905653a471ef5f46b63
pkulik0 <70904851+pkulik0@users.noreply.github.com>
2024-10-04 16:58:56 -0500

    Add image capturing

--- client/capture.cxx
+++ client/capture.cxx
@@ -2,6 +2,9 @@
 
 #include <QPainter>
 #include <QApplication>
+#include <QTimer>
+#include <QBuffer>
+#include <QByteArray>
 
 CaptureWidget::CaptureWidget(QWidget *parent) : QWidget(parent), is_drawing_(false), quit_hotkey_(std::make_unique<QHotkey>(QKeySequence("Escape"))) {
     setWindowFlags(Qt::Window | Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::ToolTip);
@@ -34,7 +37,23 @@ auto CaptureWidget::mouseReleaseEvent(QMouseEvent *event) -> void {
     end_ = event->pos();
     is_drawing_ = false;
     update();
+
+    hide();
+    QTimer::singleShot(200, this, &CaptureWidget::capture);
 }
+
+auto CaptureWidget::capture() const -> void {
+    auto* screen = QApplication::screenAt(start_);
+    const auto rect = QRect(start_, end_).normalized();
+    const auto pixmap = screen->grabWindow(0, rect.x(), rect.y(), rect.width(), rect.height());
+
+    QByteArray bytes;
+    QBuffer buffer(&bytes);
+    buffer.open(QIODevice::WriteOnly);
+    pixmap.save(&buffer, "PNG");
+    qDebug() << "Captured" << bytes.size() << "bytes";
+}
+
 
 auto CaptureWidget::paintEvent(QPaintEvent *event) -> void {
     QPainter painter(this);
